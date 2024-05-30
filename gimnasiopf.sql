7c08a4eb0b608924cbc1368c09768a8d2bb26715
BrayanJL <brayan.j.m.lucero@gmail.com>
2024-05-30 10:45:51 -0300

    Initial commit

---
+++ gimnasiopf.sql
@@ -0,0 +1,162 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 29-05-2024 a las 18:22:37
+-- Versión del servidor: 10.4.32-MariaDB
+-- Versión de PHP: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `gimnasiopf`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `clase`
+--
+
+CREATE TABLE `clase` (
+  `ID_Clase` int(11) NOT NULL,
+  `ID_Entrenador` int(11) NOT NULL,
+  `Horario` time NOT NULL,
+  `Nombre` varchar(32) NOT NULL,
+  `Capacidad` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `entrenador`
+--
+
+CREATE TABLE `entrenador` (
+  `ID_Entrenador` int(11) NOT NULL,
+  `DNI` int(11) NOT NULL,
+  `Nombre` varchar(32) NOT NULL,
+  `Apellido` varchar(32) NOT NULL,
+  `Especialidad` varchar(64) NOT NULL,
+  `Disponibilidad` varchar(32) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `membresía`
+--
+
+CREATE TABLE `membresía` (
+  `ID_Membresía` int(11) NOT NULL,
+  `ID_Socio` int(11) NOT NULL,
+  `Tipo` varchar(32) NOT NULL,
+  `Fecha_Inicio` date NOT NULL,
+  `Fecha_Fin` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `socio`
+--
+
+CREATE TABLE `socio` (
+  `ID_Socio` int(11) NOT NULL,
+  `DNI` int(10) UNSIGNED NOT NULL,
+  `Nombre` varchar(32) NOT NULL,
+  `Apellido` varchar(32) NOT NULL,
+  `Edad` int(10) UNSIGNED NOT NULL,
+  `Correo` varchar(64) NOT NULL,
+  `Teléfono` varchar(32) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `clase`
+--
+ALTER TABLE `clase`
+  ADD PRIMARY KEY (`ID_Clase`),
+  ADD KEY `entrenador_fk` (`ID_Entrenador`);
+
+--
+-- Indices de la tabla `entrenador`
+--
+ALTER TABLE `entrenador`
+  ADD PRIMARY KEY (`ID_Entrenador`),
+  ADD UNIQUE KEY `dni_unico_entrenador` (`DNI`);
+
+--
+-- Indices de la tabla `membresía`
+--
+ALTER TABLE `membresía`
+  ADD PRIMARY KEY (`ID_Membresía`),
+  ADD KEY `socio_fk` (`ID_Socio`);
+
+--
+-- Indices de la tabla `socio`
+--
+ALTER TABLE `socio`
+  ADD PRIMARY KEY (`ID_Socio`),
+  ADD UNIQUE KEY `dni_unico_socio` (`DNI`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `clase`
+--
+ALTER TABLE `clase`
+  MODIFY `ID_Clase` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `entrenador`
+--
+ALTER TABLE `entrenador`
+  MODIFY `ID_Entrenador` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `membresía`
+--
+ALTER TABLE `membresía`
+  MODIFY `ID_Membresía` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `socio`
+--
+ALTER TABLE `socio`
+  MODIFY `ID_Socio` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `clase`
+--
+ALTER TABLE `clase`
+  ADD CONSTRAINT `clase_fk_entrenador` FOREIGN KEY (`ID_Entrenador`) REFERENCES `entrenador` (`ID_Entrenador`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+--
+-- Filtros para la tabla `membresía`
+--
+ALTER TABLE `membresía`
+  ADD CONSTRAINT `membresía_fk_socio` FOREIGN KEY (`ID_Socio`) REFERENCES `socio` (`ID_Socio`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
